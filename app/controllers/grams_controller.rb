e93c12c8278f0ad4146d4926843b97f0cdf6ff5d
Zayn Rahman <zaynrahn@gmail.com>
2017-06-26 02:35:41 +0000

    Secured edit & destroy for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
   end
@@ -25,19 +25,27 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram. update_attributes(gram_params)
-
     if @gram.valid?
       redirect_to root_path
     else
       return render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
   end
 
   private
@@ -46,7 +54,14 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found =(', status: :not_found
+  def render_not_found(status = :not_found)
+    render plain: "#{status.to_s.titleize} =(", status: status
   end
 end
+
+
+
+
+
+
+
