d0807ce3afc53f37d608a1db52600be1015dc221
Zayn Rahman <zaynrahn@gmail.com>
2017-06-26 01:42:28 +0000

    Added edit and update feature for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -17,13 +17,36 @@ class GramsController < ApplicationController
     end
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram. update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
 
-  #add helper method to verify user is allowed to upload grams?
-  #def require_authorized_for_current_user
-  #end
+  def render_not_found
+    render plain: 'Not Found =(', status: :not_found
+  end
 end
